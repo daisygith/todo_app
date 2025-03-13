dab10d000c23b1fab5ff1234e8917c396ec034ef
daisygith <daisygith@users.noreply.github.com>
2025-03-13 18:31:56 +0100

    add table

--- client/todo_app/src/app/todo/components/todo.component.ts
+++ client/todo_app/src/app/todo/components/todo.component.ts
@@ -1,27 +1,85 @@
-import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { TodoService } from '../service/todo.service';
 import { Task } from '../model/task';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+  MatTableDataSource,
+} from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
+    DatePipe,
+  ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class TodoComponent implements OnInit {
   private _todoService = inject(TodoService);
+  private _fb: FormBuilder = inject(FormBuilder);
 
   public tasks: Task[] | undefined;
+  data: any;
+
+  public todoGroup!: FormGroup;
+
+  @ViewChild(MatTable) table: MatTable<Task> | undefined;
+
+  dataSource = new MatTableDataSource<Task>([]);
+  displayedColumns = ['description', 'status', 'finishDate'];
 
   ngOnInit(): void {
+    this.buildForm();
     this.getTasks();
   }
 
+  // removeData() {
+  //   this.dataSource.pop();
+  //   this.table.renderRows();
+  // }
+
   getTasks(): void {
     this._todoService.findAllTasks().subscribe({
-      next: (value) => (this.tasks = value),
+      next: (value) => (this.dataSource.data = value),
+    });
+  }
+
+  public buildForm() {
+    this.todoGroup = this._fb.group({
+      id: [null],
+      description: [null],
+      status: [null],
+      finishDate: [null],
     });
   }
 }
--- client/todo_app/src/app/todo/model/task.ts
+++ client/todo_app/src/app/todo/model/task.ts
@@ -2,5 +2,5 @@ export interface Task {
   id: number;
   description: string;
   status: string;
-  finish_date: Date;
+  finishDate: Date;
 }
