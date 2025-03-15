39c7a91b062ba16cd44b064871cdb3d984484a49
daisygith <daisygith@users.noreply.github.com>
2025-03-15 18:29:15 +0100

    add method add and update task; change appearance table;

--- client/todo_app/src/app/todo/components/todo.component.ts
+++ client/todo_app/src/app/todo/components/todo.component.ts
@@ -3,7 +3,6 @@ import {
   Component,
   inject,
   OnInit,
-  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 import { TodoService } from '../service/todo.service';
@@ -11,6 +10,7 @@ import { Task } from '../model/task';
 import {
   FormArray,
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
@@ -28,14 +28,18 @@ import {
   MatTable,
   MatTableDataSource,
 } from '@angular/material/table';
-import { MatHint, MatInput, MatSuffix } from '@angular/material/input';
-import { MatMiniFabButton } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import {
-  MatDatepicker,
-  MatDatepickerInput,
-  MatDatepickerToggle,
-} from '@angular/material/datepicker';
+  MatFormField,
+  MatInput,
+  MatInputModule,
+} from '@angular/material/input';
+import { MatIconButton, MatMiniFabButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import { DatePipe, NgForOf, NgIf } from '@angular/common';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { DateRange, MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 @Component({
   selector: 'app-todo',
   standalone: true,
@@ -51,15 +55,20 @@ import {
     MatColumnDef,
     MatHeaderCell,
     MatHeaderCellDef,
-    MatInput,
+    MatInputModule,
     MatMiniFabButton,
     MatIconModule,
-    MatDatepickerInput,
-    MatHint,
-    MatDatepickerToggle,
-    MatSuffix,
-    MatDatepicker,
+    MatFormField,
+    MatIconButton,
+    MatTooltip,
+    NgIf,
+    MatSelect,
+    MatOption,
+    NgForOf,
+    MatDatepickerModule,
+    DatePipe,
   ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
   encapsulation: ViewEncapsulation.None,
@@ -71,47 +80,63 @@ export class TodoComponent implements OnInit {
 
   public tasks!: Task[];
   data: any;
+  isLoading = true;
+  id: number | undefined;
+  isNew: boolean = false;
+
+  minDate = new Date(new Date().setHours(0, 0, 0, 0));
 
   public todoGroup!: FormGroup;
 
-  @ViewChild(MatTable) table: MatTable<Task> | undefined;
-
-  // dataSource = new MatTableDataSource<Task>([]);
   dataSource = new MatTableDataSource<Task>([]);
   displayedColumns = ['description', 'status', 'finishDate', 'actions'];
 
   ngOnInit(): void {
+    this.buildForm();
     this.getTasks();
+    this.isLoading = false;
+    this.isNew = !this.id;
   }
 
-  constructor(private fb: FormBuilder) {
-    this.todoGroup = this.fb.group({
-      tableRows: this.fb.array([]),
+  public statusTask: string[] = ['New', 'In progress', 'Done'];
+
+  public buildForm() {
+    this.todoGroup = this._fb.group({
+      tableRows: this._fb.array([]),
     });
-    this.addRow();
   }
 
-  buildForm(): FormGroup {
-    return this.fb.group({
-      id: [''],
-      description: ['', [Validators.required]],
-      status: [''],
-      finishDate: [''],
+  addTasksFormArray() {
+    this.tasks?.forEach((task) =>
+      this.getFormControls.push(this.createTableRowItemFormGroup(task)),
+    );
+  }
+
+  createTableRowItemFormGroup(task?: Task) {
+    return this._fb.group({
+      id: new FormControl(task?.id),
+      description: new FormControl(task?.description, [Validators.required]),
+      status: new FormControl(task?.status, [Validators.required]),
+      finishDate: new FormControl(task?.finishDate),
+      action: new FormControl('existingRecord'),
+      isEdited: new FormControl(false),
+      isNewRow: new FormControl(false),
     });
   }
 
   get getFormControls() {
     return this.todoGroup.get('tableRows') as FormArray;
   }
+  get tableRowsValue() {
+    return this.getFormControls.value;
+  }
 
   getTasks(): void {
     this._todoService.findAllTasks().subscribe({
       next: (value) => {
-        value?.forEach((item) => {
-          this.addRow();
-        });
-        this.getFormControls.patchValue(value);
-        this.dataSource.data = value;
+        this.tasks = value;
+        this.addTasksFormArray();
+        this.updateDataSource(this.tasks);
       },
     });
   }
@@ -130,12 +155,67 @@ export class TodoComponent implements OnInit {
     );
   }
 
-  addRow() {
-    const newForm = this.buildForm();
+  addNewRow() {
+    const newForm = this.initiateVOForm();
     const control = this.todoGroup.get('tableRows') as FormArray;
     control.push(newForm);
-    let index = control.length;
     this.dataSource.data = [...this.dataSource.data, newForm.value];
-    console.log(index);
+  }
+
+  editTask(i: number) {
+    this.getFormControls?.at(i)?.get('isEdited')?.patchValue(true);
+  }
+
+  saveTask(i: number) {
+    this.getFormControls?.at(i)?.get('isEdited')?.patchValue(false);
+
+    const rowForm = this.getFormControls?.at(i);
+
+    if (!rowForm.valid) {
+      return;
+    }
+    if (!rowForm?.value?.id) {
+      this._todoService.addTask(rowForm?.value).subscribe({
+        next: (data) => {
+          rowForm.patchValue(data);
+          this.tasks.push(data);
+          this.updateDataSource(this.tasks);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    } else {
+      this._todoService.updateTask(rowForm?.value).subscribe({
+        next: (data) => {
+          this.tasks[i] = data;
+          rowForm.patchValue(data);
+          this.updateDataSource(this.tasks);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
+
+  cancelTask(i: number) {
+    this.getFormControls?.at(i)?.get('isEdited')?.patchValue(false);
+  }
+
+  initiateVOForm(): FormGroup {
+    return this._fb.group({
+      id: new FormControl(null),
+      description: new FormControl('', [Validators.required]),
+      status: new FormControl('', [Validators.required]),
+      finishDate: new FormControl('', [Validators.required]),
+      action: new FormControl('newRecord'),
+      isEdited: new FormControl(true),
+      isNewRow: new FormControl(true),
+    });
+  }
+
+  private updateDataSource(data: Task[]) {
+    this.dataSource.data = data;
   }
 }
--- client/todo_app/src/app/todo/service/todo.service.ts
+++ client/todo_app/src/app/todo/service/todo.service.ts
@@ -11,19 +11,27 @@ export class TodoService {
 
   //get all tasks
   findAllTasks(): Observable<Task[]> {
-    return this._http
-      .get<Task[]>(`${this._apiUrl}`)
-      .pipe(
-        map((data) =>
-          data.map((item) => ({
-            ...item,
-            finishDate: new Date(item.finishDate),
-          })),
-        ),
-      );
+    return this._http.get<Task[]>(`${this._apiUrl}`).pipe(
+      map((data) =>
+        data.map((item) => ({
+          ...item,
+          finishDate: new Date(item.finishDate),
+        })),
+      ),
+    );
   }
 
-  //delete teask by ID
+  //add new task
+  addTask(task: Task): Observable<Task> {
+    return this._http.post<Task>(`${this._apiUrl}`, task);
+  }
+
+  //update task by id
+  updateTask(task: Task): Observable<Task> {
+    return this._http.put<Task>(`${this._apiUrl}/${task.id}`, task);
+  }
+
+  //delete task by ID
   deleteTaskById(taskId: number): Observable<void> {
     return this._http.delete<void>(`${this._apiUrl}/${taskId}`);
   }
